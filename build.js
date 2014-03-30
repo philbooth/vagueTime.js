a36ada5594c800d57e7e9121cbfdf8099e00ebf4
Phil Booth <pmbooth@gmail.com>
2014-03-30 15:47:07 +0100

    Add build script.

---
+++ build.js
@@ -0,0 +1,125 @@
+#!/usr/bin/env node
+
+'use strict';
+
+var cli, fs, path, srcPath, libPath, languagesPath, rwrr;
+
+cli = require('commander');
+fs = require('fs');
+path = require('path');
+
+srcPath = path.resolve(__dirname, 'src');
+libPath = path.resolve(__dirname, 'lib');
+languagesPath = path.resolve(srcPath, 'languages');
+
+rwrr = parseInt('644', 8); // Octal literals not allowed in strict mode
+
+build();
+
+function build() {
+    var languages;
+
+    parseCommandLine();
+
+    languages = getLanguages();
+
+    checkLanguages(languages);
+
+    writeLibrary(
+        injectLanguageModules(
+            readMainModule(),
+            readLanguageModules(languages)
+        ),
+        isUniversalBuild() ? '' : '-' + languages.join('-')
+    );
+}
+
+function parseCommandLine () {
+    cli.usage('[options]')
+       .option('-l, --language <languages>', 'comma-separated list of 2-character language codes, or `all` for all defined (default)');
+}
+
+
+function getLanguages () {
+    if (isUniversalBuild()) {
+        return getAllLanguages();
+    }
+
+    return cli.language.split(',');
+}
+
+function isUniversalBuild () {
+    return cli.language === 'all' || !cli.language;
+}
+
+function getAllLanguages () {
+    console.log('Detecting languages...');
+
+    return fs.readdirSync(languagesPath).map(function (filename) {
+        var extensionIndex = filename.lastIndexOf('.js'), language;
+
+        if (extensionIndex === -1) {
+            language = filename;
+        } else {
+            language = filename.substr(0, extensionIndex);
+        }
+
+        console.log('Detected language: ' + language);
+
+        return language;
+    });
+}
+
+function checkLanguages (languages) {
+    if (languages.length === 0) {
+        throw new Error('I see no work.');
+    }
+}
+
+function readMainModule () {
+    return fs.readFileSync(path.resolve(srcPath, 'vagueTime.js'), 'utf8');
+}
+
+function readLanguageModules (languages) {
+    var modules = new Array(languages.length);
+
+    console.log('Reading language modules...');
+
+    languages.forEach(function (language, index) {
+        modules[index] = fs.readFileSync(path.resolve(languagesPath, language + '.js'), 'utf8');
+
+        console.log('Read language module: ' + language);
+    });
+
+    return modules;
+}
+
+function injectLanguageModules (main, modules) {
+    var split = main.split('//#include languages');
+
+    if (split.length !== 2) {
+        throw new Error('Injection point error.');
+    }
+
+    return split[0] + modules.join(',\n') + split[1];
+}
+
+function writeLibrary (library, suffix) {
+    var outputPath;
+
+    console.log('Writing library...');
+
+    outputPath = path.resolve(libPath, 'vagueTime' + suffix + '.js');
+
+    fs.writeFileSync(
+        outputPath,
+        library,
+        {
+            encoding: 'utf8',
+            mode: rwrr
+        }
+    );
+
+    console.log('Written library to: ' + outputPath);
+}
+
