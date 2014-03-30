5a5da81e2b32ecd892464d3f2dc3c2704947daf1
Phil Booth <pmbooth@gmail.com>
2014-03-30 13:03:35 +0100

    Add language modules.

---
+++ src/languages/en.js
@@ -0,0 +1,21 @@
+{
+    year: [ 'year', 'years' ],
+    month: [ 'month', 'months' ],
+    week: [ 'week', 'weeks' ],
+    day: [ 'day', 'days' ],
+    hour: [ 'hour', 'hours' ],
+    minute: [ 'minute', 'minutes' ],
+
+    past: function (vagueTime, unit) {
+        return vagueTime + ' ' + unit + ' ago';
+    },
+
+    future: function (vagueTime, unit) {
+        return 'in ' + vagueTime + ' ' + unit;
+    },
+
+    defaults: {
+        past: 'just now',
+        future: 'soon'
+    }
+}
---
+++ src/languages/nl.js
@@ -0,0 +1,21 @@
+{
+    year: [ 'jaar', 'jaar' ],
+    month: [ 'maand', 'maanden' ],
+    week: [ 'week', 'weken' ],
+    day: [ 'dag', 'dagen' ],
+    hour: [ 'uur', 'uur' ],
+    minute: [ 'minuut', 'minuten' ],
+
+    past: function (vagueTime, unit) {
+        return vagueTime + ' ' + unit + ' geleden';
+    },
+
+    future: function (vagueTime, unit) {
+        return 'over ' + vagueTime + ' ' + unit;
+    },
+
+    defaults: {
+        past: 'juist nu',
+        future: 'binnenkort'
+    }
+}
