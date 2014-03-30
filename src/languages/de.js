c9f784fbda85c2032a5986b77f1ede2cf18eeb9f
Phil Booth <pmbooth@gmail.com>
2014-03-30 16:17:03 +0100

    Increase language indentation.

--- src/languages/de.js
+++ src/languages/de.js
@@ -1,21 +1,21 @@
-{
-    year: [ 'Jahr', 'Jahren' ],
-    month: [ 'Monat', 'Monaten' ],
-    week: [ 'Woche', 'Wochen' ],
-    day: [ 'Tag', 'Tagen' ],
-    hour: [ 'Stunde', 'Stunden' ],
-    minute: [ 'Minute', 'Minuten' ],
-
-    past: function (vagueTime, unit) {
-        return 'vor ' + vagueTime + ' ' + unit;
-    },
-
-    future: function (vagueTime, unit) {
-        return 'in ' + vagueTime + ' ' + unit;
-    },
-
-    defaults: {
-        past: 'jetzt gerade',
-        future: 'bald'
-    }
-}
+        {
+            year: [ 'Jahr', 'Jahren' ],
+            month: [ 'Monat', 'Monaten' ],
+            week: [ 'Woche', 'Wochen' ],
+            day: [ 'Tag', 'Tagen' ],
+            hour: [ 'Stunde', 'Stunden' ],
+            minute: [ 'Minute', 'Minuten' ],
+        
+            past: function (vagueTime, unit) {
+                return 'vor ' + vagueTime + ' ' + unit;
+            },
+        
+            future: function (vagueTime, unit) {
+                return 'in ' + vagueTime + ' ' + unit;
+            },
+        
+            defaults: {
+                past: 'jetzt gerade',
+                future: 'bald'
+            }
+        }
