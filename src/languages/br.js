8a22c8dacc0ab81d2c25712592615e17e1d1efdb
Phil Booth <pmbooth@gmail.com>
2015-01-14 13:46:53 +0000

    Indentation.

--- src/languages/br.js
+++ src/languages/br.js
@@ -1,21 +1,21 @@
-br: {
-        year: [ 'ano', 'anos' ],
-        month: [ 'mês', 'meses' ],
-        week: [ 'semana', 'semanas' ],
-        day: [ 'dia', 'dias' ],
-        hour: [ 'hora', 'horas' ],
-        minute: [ 'minuto', 'minutos' ],
+        br: {
+            year: [ 'ano', 'anos' ],
+            month: [ 'mês', 'meses' ],
+            week: [ 'semana', 'semanas' ],
+            day: [ 'dia', 'dias' ],
+            hour: [ 'hora', 'horas' ],
+            minute: [ 'minuto', 'minutos' ],
 
-        past: function (vagueTime, unit) {
-            return vagueTime + ' ' + unit + ' atrás';
-        },
+            past: function (vagueTime, unit) {
+                return vagueTime + ' ' + unit + ' atrás';
+            },
 
-        future: function (vagueTime, unit) {
-            return 'em ' + vagueTime + ' ' + unit;
-        },
+            future: function (vagueTime, unit) {
+                return 'em ' + vagueTime + ' ' + unit;
+            },
 
-        defaults: {
-            past: 'agora mesmo',
-            future: 'em breve'
+            defaults: {
+                past: 'agora mesmo',
+                future: 'em breve'
+            }
         }
-    }
